7f9719a61e42f4dafc6976494dabafa9053a8a1e
rajnish1999 <48800157+rajnish1999@users.noreply.github.com>
2019-10-15 20:04:20 +0530

    second

--- stratergy.js
+++ stratergy.js
@@ -19,6 +19,40 @@ passport.use(new LocalStratergy((username, password, done) => {
   )
 );
 
+passport.use( new FacebookStratergy({
+    clientID: 'fbID',
+    clientSecret: 'fbSecret',
+    callbackURL: '',
+    profileFields: ['emails', 'displayName', 'picture.type(large)']
+},
+(accessToken, refreshToken, profile, done) => {
+    connectdb('empmanag')
+        .then(db => db.collection('users').find({ email: profile.emails[0].value }))
+        .then(user => user.toArray())
+        .then((user) => {
+            if(user.length == 0) {
+                let fbuser = {
+                    dp: profile.photos[0].value,
+                    email: profile.emails[0].value,
+                    username: profile.displayName,
+                    fbAccessToken: accessToken
+                }
+                connectdb('blogportal')
+                    .then(db => db.collection('users').insertOne(fbuser))
+                    .then(fbuser => {
+                        console.log(fbuser.ops)
+                        done(null, fbuser.ops)
+                    })
+                    .catch(done)
+            }
+            else{
+                done(null, user)
+            }
+        })
+        .catch(done)
+}))
+
+
 passport.serializeUser((user, done) => {
     done(null, user)
 })
